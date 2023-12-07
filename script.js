ebde6a71687db176b6a6936f95cacd2d78eeb387
jacobhcs <jaharr1999@gmail.com>
2023-12-07 15:59:57 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+var editName = document.querySelector('#editname');
+var nameList = ['Harry Potter','Sir Grindel','Cersei Lannister','Lord Farquad','Jane Doe'];
+var number = document.querySelector("#decrease");
+var count = 499;
+
+function newName(){
+  var randName = Math.floor(Math.random() * 4);
+  editName.innerText = nameList[randName];
+}
+
+var user = document.querySelector('#remove');
+
+function removeUser(){
+  if(user){
+  user.remove();
+} else {
+  console.log('no user here.');
+}
+  number.innerText = count + ' Your Connections';
+  count--;
+}
+
+// function decreaseNumber(){
+//   number.innerText = count;
+//   count--;
+// }
