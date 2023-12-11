ab3ed1e5f52e4cc352f59e9973c610424214b112
jacobhcs <jaharr1999@gmail.com>
2023-12-10 23:45:44 -0500

    validates clean and different script code.

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
 var editName = document.querySelector('#editname');
-var nameList = ['Harry Potter','Sir Grindel','Cersei Lannister','Lord Farquad','Jane Doe'];
-var number = document.querySelector("#decrease");
-var count = 499;
-
-function newName(){
-  var randName = Math.floor(Math.random() * 4);
-  editName.innerText = nameList[randName];
-}
-
+var nameList = ['Harry Potter', 'Sir Grindel', 'Jane Lannister', 'Lord Farquad'];
+var number = document.querySelector('#decrease');
+var number2 = document.querySelector('#increase');
 var user = document.querySelector('#remove');
+var usertwo = document.querySelector('#remove2');
 
-function removeUser(){
-  if(user){
+function newName() {
+  if (editName.innerText == 'Jane Doe') {
+    editName.innerText = nameList[0];
+  } else if (editName.innerText == nameList[0]) {
+    editName.innerText = nameList[1];
+  } else if (editName.innerText == nameList[1]) {
+    editName.innerText = nameList[2];
+  } else if (editName.innerText == nameList[2]) {
+    editName.innerText = nameList[3];
+  } else {
+    editName.innerText = 'Jane Doe';
+  }
+}
+
+function acceptUser() {
   user.remove();
-} else {
-  console.log('no user here.');
-}
-  number.innerText = count;
-  count--;
+  number.innerText--;
+  number2.innerText++;
 }
 
-// function decreaseNumber(){
-//   number.innerText = count;
-//   count--;
-// }
+function denyUser() {
+  user.remove();
+  number.innerText--;
+}
+
+function acceptUser2() {
+  usertwo.remove();
+  number.innerText--;
+  number2.innerText++;
+}
+
+function denyUser2() {
+  usertwo.remove();
+  number.innerText--;
+}
